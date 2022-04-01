8eee39a6907149454eed461c53312339b5fe3147
Samuel <smarouba2@gmail.com>
2022-04-01 13:22:46 -0500

    commit tree c4530df50ce5a46abee019e95a77ad30077dfcf0

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string){   
+return string.toUpperCase();
+} 
+function whisper(string){
+    return string.toLowerCase();
+}
+function logShout (string){
+    console.log(string.toUpperCase());
+}
+ function logWhisper(string){
+     console.log(string.toLowerCase());
+ }
+function sayHiToHeadphonedRoommate(greeting){
+    if(greeting === greeting.toLowerCase()){
+       return "I can't hear you!" 
+    }
+    if (greeting === "Let's have dinner together!") {
+    return "I would love to!"
+}
+else if (greeting === greeting.toUpperCase());
+return "YES INDEED!"
+}
+
+
+
